0daa8aa604ab740c6f4091cc53da08f5b3c4e496
Grant Hutchinson <grant@splorp.com>
2014-03-17 17:41:28 -0600

    Added favicon

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -7,6 +7,7 @@
   <meta name="description" content="<?php echo html($site->description()) ?>" />
   <meta name="keywords" content="<?php echo html($site->keywords()) ?>" />
   <meta name="robots" content="index, follow" />
+  <link rel="shortcut icon" href="/favicon.ico" />
 
   <?php echo css('assets/styles/styles.css') ?>
 
