caa572ee4188725b23d43bef74825e50fd31873a
Grant Hutchinson <grant@splorp.com>
2014-03-18 16:58:06 -0600

    Added navigation for previous and next terms

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,24 +1,22 @@
 <?php snippet('header') ?>
 <?php snippet('menu') ?>
-<?php snippet('submenu') ?>
+<?php $page->isChildOf($pages->find('terms')) ? snippet('prevnext') : snippet('submenu'); ?>
 
 <section class="content">
-
   <article>
     <h1><?php echo html($page->title()) ?></h1>
     <?php echo kirbytext($page->text()) ?>
 
-	<?php if($page->related()): ?>
-	<h2>Related Terms</h2>
-	<ul>
-		<?php foreach(related($page->related()) as $related): ?>
-	  	<li><a href="<?php echo $related->url() ?>"><?php echo html($related->title()) ?></a></li>
-  		<?php endforeach ?>
-  	</ul>
-  	<?php endif ?>
-  	
-  </article>
+    <?php if($page->related()): ?>
+      <h2>Related Terms</h2>
+      <ul>
+        <?php foreach(related($page->related()) as $related): ?>
+          <li><a href="<?php echo $related->url() ?>"><?php echo html($related->title()) ?></a></li>
+        <?php endforeach ?>
+      </ul>
+    <?php endif ?>
 
+  </article>
 </section>
 
 <?php snippet('footer') ?>
