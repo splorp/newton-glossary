bfd76ee36c1373463854a19c3d5c7c0b651fa19f
Grant Hutchinson <grant@splorp.com>
2014-04-01 14:41:11 -0600

    Alphabetical grouping of terms and sources

--- site/snippets/submenu.php
+++ site/snippets/submenu.php
@@ -1,16 +1,21 @@
 <?php 
 
-// find the open/active page on the first level
+// Find the open or active page on the first level
 $open  = $pages->findOpen();
 $items = ($open) ? $open->children() : false; 
+// Set up alphabetise plugin
+$alphabetise = alphabetise($page->children()->sortby('title'), array('key' => 'title'));
 
 ?>
 <?php if($items && $items->count()): ?>
-<nav class="submenu">
+<nav class="submenu alphabetical">
+<?php foreach($alphabetise as $letter => $items): ?>
+  <h2><?php echo strtoupper($letter) ?></h2>
   <ul>
-    <?php foreach($items AS $item): ?>
-    <li><a<?php echo ($item->isOpen()) ? ' class="active"' : '' ?> href="<?php echo $item->url() ?>"><?php echo html($item->title()) ?></a></li>
+    <?php foreach($items as $item): ?>
+    <li><a href="<?php echo $item->url() ?>"><?php echo html($item->title()) ?></a></li>
     <?php endforeach ?>            
   </ul>
+<?php endforeach ?>
 </nav>
 <?php endif ?>
