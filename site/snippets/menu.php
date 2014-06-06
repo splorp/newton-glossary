a7255cc3420a5cd5e85981244ead011ae0855004
Grant Hutchinson <grant@splorp.com>
2014-06-06 16:57:39 -0600

    Case consistency

--- site/snippets/menu.php
+++ site/snippets/menu.php
@@ -1,6 +1,6 @@
 <nav class="menu">
   <ul>
-    <?php foreach($pages->visible() AS $p): ?>
+    <?php foreach($pages->visible() as $p): ?>
     <li><a<?php echo ($p->isOpen()) ? ' class="active"' : '' ?> href="<?php echo $p->url() ?>"><?php echo html($p->title()) ?></a></li>
     <?php endforeach ?>
   </ul>
