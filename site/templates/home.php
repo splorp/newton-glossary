d567c0c6e0158a6a1f790b9a37654c53da0b9b5a
Grant Hutchinson <grant@splorp.com>
2015-12-27 12:40:34 -0700

    Added source count to home page

--- site/templates/home.php
+++ site/templates/home.php
@@ -8,9 +8,11 @@
     <h1><?php echo html($page->title()) ?></h1>
     <?php
 		$t = $pages->findByUID('terms')->children()->count();
+		$s = $pages->findByUID('sources')->children()->count();
 	    $p = str_replace('_$t', $t, $page->text());
+	    $pp = str_replace('_$s', $s, $p);
 	?>
-    <?php echo kirbytext($p) ?>
+    <?php echo kirbytext($pp) ?>
   </article>
 
 </section>
