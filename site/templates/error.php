93fe3b2219341f0f51d3b3e4154ffefef99f81cb
Grant Hutchinson <grant@splorp.com>
2016-01-13 11:40:41 -0700

    Formatting and newlines

--- site/templates/error.php
+++ site/templates/error.php
@@ -2,12 +2,10 @@
 <?php snippet('menu') ?>
 
 <section class="content">
-
-  <article>
-    <h1><?php echo html($page->title()) ?></h1>
-    <?php echo kirbytext($page->text()) ?>
-  </article>
-
+	<article>
+		<h1><?php echo html($page->title()) ?></h1>
+		<?php echo kirbytext($page->text()) ?>
+	</article>
 </section>
 
 <?php snippet('footer') ?>
--- site/templates/home.php
+++ site/templates/home.php
@@ -3,18 +3,16 @@
 <?php snippet('submenu') ?>
 
 <section class="content">
-
-  <article>
-    <h1><?php echo html($page->title()) ?></h1>
-    <?php
+	<article>
+		<h1><?php echo html($page->title()) ?></h1>
+		<?php
 		$t = $pages->findByUID('terms')->children()->count();
 		$s = $pages->findByUID('sources')->children()->count();
-	    $p = str_replace('_$t', $t, $page->text());
-	    $pp = str_replace('_$s', $s, $p);
-	?>
-    <?php echo kirbytext($pp) ?>
-  </article>
-
+		$p = str_replace('_$t', $t, $page->text());
+		$pp = str_replace('_$s', $s, $p);
+		?>
+		<?php echo kirbytext($pp) ?>
+	</article>
 </section>
 
 <?php snippet('footer') ?>
