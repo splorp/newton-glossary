fc07a1f9acd538fbd7067208b04552b8611df503
Grant Hutchinson <grant@splorp.com>
2016-09-29 16:23:37 -0600

    Page navigation title text is now generic

--- site/snippets/prevnext.php
+++ site/snippets/prevnext.php
@@ -1,10 +1,10 @@
 <nav class="submenu">
 	<ul>
 	<?php if($page->hasPrev() && !$page->prev()->isErrorPage()): ?>
-		<li><a href="<?php echo $page->prev()->url() ?>" title="View the previous term">Previous</a></li>
+		<li><a href="<?php echo $page->prev()->url() ?>" title="Previously: <?php echo $page->prev()->title() ?>">Previous</a></li>
 	<?php endif ?>
 	<?php if($page->hasNext() && !$page->next()->isErrorPage()): ?>
-		<li><a href="<?php echo $page->next()->url() ?>" title="View the next term">Next</a></li>
+		<li><a href="<?php echo $page->next()->url() ?>" title="Next up: <?php echo $page->next()->title() ?>">Next</a></li>
 	<?php endif ?>
 	</ul>
 </nav>
