dfe8cd271d55ca100669dedfbeae0a94f2aa2c81
Grant Hutchinson <grant@splorp.com>
2016-10-20 17:35:42 -0600

    Updated Kirby to 2.3.2

--- site/templates/default.php
+++ site/templates/default.php
@@ -11,19 +11,19 @@
 
 		<?php $page->isChildOf($pages->find('terms')) || $page->isChildOf($pages->find('sources')) ? '' : snippet('submenu'); ?>
 	
-		<?php if($page->source()): ?>
+		<?php if($page->content()->has('Source')): ?>
 		<h2>Sources</h2>
 		<ul class="src">
-			<?php foreach(related($page->source()) as $source): ?>
+			<? $n=0; foreach($page->source()->pages() as $source): $n++; ?>
 			<li><a href="<?php echo $source->url() ?>"><?php echo html($source->title()) ?></a></li>
 			<?php endforeach ?>
 		</ul>
 		<?php endif ?>
 
-		<?php if($page->related()): ?>
+		<?php if($page->content()->has('Related')): ?>
 		<h2>Related Terms</h2>
 		<ul class="src">
-			<?php foreach(related($page->related()) as $related): ?>
+			<? $n=0; foreach($page->related()->pages() as $related): $n++; ?>
 			<li><a href="<?php echo $related->url() ?>"><?php echo html($related->title()) ?></a></li>
 			<?php endforeach ?>
 		</ul>
--- site/templates/home.php
+++ site/templates/home.php
@@ -6,12 +6,13 @@
 	<article>
 		<h1><?php echo html($page->title()) ?></h1>
 		<?php
-		$t = $pages->findByUID('terms')->children()->count();
-		$s = $pages->findByUID('sources')->children()->count();
+		$t = page('terms')->children()->count();
+		$s = page('sources')->children()->count();;
 		$p = str_replace('_$t', $t, $page->text());
 		$pp = str_replace('_$s', $s, $p);
 		?>
 		<?php echo kirbytext($pp) ?>
+		
 	</article>
 </section>
 
