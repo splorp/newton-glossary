ca6ca5f9187545957b8abc65a3c09aa551f85509
Grant Hutchinson <grant@splorp.com>
2016-10-25 14:18:47 -0600

    Highlight menu item when search page active

--- site/snippets/menu.php
+++ site/snippets/menu.php
@@ -5,9 +5,9 @@
 <nav class="menu">
 	<ul>
 	<?php foreach($pages->visible() as $p): ?>
-		<li><a<?php echo ($p->isOpen()) ? ' class="active"' : '' ?> href="<?php echo $p->url() ?>"><?php echo html($p->title()) ?></a></li>
+		<li><a href="<?php echo $p->url() ?>"<?php echo ($p->isOpen()) ? ' class="active"' : '' ?>><?php echo html($p->title()) ?></a></li>
 	<?php endforeach ?>
 		<li><a href="<?php echo $random->url() ?>">Random</a></li>
-		<li><a href="/search">Search</a></li>
+		<li><a href="/search"<?php echo (page('search')->isOpen()) ? ' class="active"' : '' ?>>Search</a></li>
 	</ul>
 </nav>
