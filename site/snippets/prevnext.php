69243d7a2d98a32b13463dd876cfa1a7a3539887
Grant Hutchinson <grant@splorp.com>
2016-10-27 15:51:10 -0600

    Disabled link states for page navigation

--- site/snippets/prevnext.php
+++ site/snippets/prevnext.php
@@ -1,10 +1,14 @@
 <nav class="submenu">
 	<ul>
-	<?php if($page->hasPrev() && !$page->prev()->isErrorPage()): ?>
+	<?php if($page->hasPrev() && !$page->prev()->isErrorPage()) : ?>
 		<li><a href="<?php echo $page->prev()->url() ?>" title="Previously: <?php echo $page->prev()->title() ?>">Previous</a></li>
+	<?php else: ?>
+		<li>Previous</li>
 	<?php endif ?>
-	<?php if($page->hasNext() && !$page->next()->isErrorPage()): ?>
+	<?php if($page->hasNext() && !$page->next()->isErrorPage()) : ?>
 		<li><a href="<?php echo $page->next()->url() ?>" title="Next up: <?php echo $page->next()->title() ?>">Next</a></li>
+	<?php else: ?>
+		<li>Next</li>
 	<?php endif ?>
 	</ul>
 </nav>
