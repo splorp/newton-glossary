85215233e62e80144ad4307af47d773e750d1c3a
Grant Hutchinson <grant@splorp.com>
2017-02-08 15:48:14 -0700

    Added contextual singular and plural subheads

--- site/templates/default.php
+++ site/templates/default.php
@@ -12,7 +12,7 @@
 		<?php $page->isChildOf($pages->find('terms')) || $page->isChildOf($pages->find('sources')) ? '' : snippet('submenu'); ?>
 	
 		<?php if($page->content()->has('Source')): ?>
-		<h2>Sources</h2>
+		<?php if($page->source()->pages()->count() > 1) { echo '<h2>Sources</h2>'; } else { echo '<h2>Source</h2>'; } ?>
 		<ul class="src">
 			<? $n=0; foreach($page->source()->pages() as $source): $n++; ?>
 			<li><a href="<?php echo $source->url() ?>"><?php echo html($source->title()) ?></a></li>
@@ -21,7 +21,7 @@
 		<?php endif ?>
 
 		<?php if($page->content()->has('Related')): ?>
-		<h2>Related Terms</h2>
+		<?php if($page->related()->pages()->count() > 1) { echo '<h2>Related Terms</h2>'; } else { echo '<h2>Related Term</h2>'; } ?>
 		<ul class="src">
 			<? $n=0; foreach($page->related()->pages() as $related): $n++; ?>
 			<li><a href="<?php echo $related->url() ?>"><?php echo html($related->title()) ?></a></li>
