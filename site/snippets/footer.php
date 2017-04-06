7185df95d5f751ad61d735284545b4b32800535e
Grant Hutchinson <grant@splorp.com>
2017-04-06 16:21:27 -0700

    Moved the about and change log links to footer, updated main navigation

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,4 +1,10 @@
 		<footer>
+			<nav>
+				<ul>
+					<li><a href="/about">About the Newton Glossary</a></li>
+					<li><a href="/changes">Change Log</a></li>
+				</ul>
+			</nav>
 			<?php echo kirbytext($site->copyright() . "<br />" . $site->colophon()) ?>
 		</footer>
 		<?php echo snippet('ga'); ?>
--- site/snippets/menu.php
+++ site/snippets/menu.php
@@ -4,7 +4,7 @@
 
 <nav class="menu">
 	<ul>
-	<?php foreach($pages->visible() as $p): ?>
+	<?php foreach($pages->visible()->not('about', 'changes') as $p): ?>
 		<li><a href="<?php echo $p->url() ?>"<?php echo ($p->isOpen()) ? ' class="active"' : '' ?>><?php echo html($p->title()) ?></a></li>
 	<?php endforeach ?>
 		<li><a href="<?php echo $random->url() ?>">Random</a></li>
