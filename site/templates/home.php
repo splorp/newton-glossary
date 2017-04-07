e0fe827ce2fc76167407ee17d04369e473831413
Grant Hutchinson <grant@splorp.com>
2017-04-06 23:40:09 -0600

    Code comments are a good thing, right?

--- site/templates/home.php
+++ site/templates/home.php
@@ -2,9 +2,12 @@
 <?php snippet('menu') ?>
 
 <?php
+	/* Nab the number of term and source pages */
 	$t = number_format(page('terms')->children()->count());
 	$s = number_format(page('sources')->children()->count());
+	/* Replace the term count placeholder in the page text */
 	$p = str_replace('_$t', $t, $page->text());
+	/* Replace the source count placeholder in the page text */
 	$pp = str_replace('_$s', $s, $p);
 ?>
 
