3d377a48a7a6cddd34b983d29fa5df99aa1e962c
Grant Hutchinson <grant@splorp.com>
2017-04-15 22:24:35 -0600

    Added ability to use different text for navigation links

--- site/snippets/menu.php
+++ site/snippets/menu.php
@@ -5,7 +5,7 @@
 <nav class="menu">
 	<ul>
 	<?php foreach($pages->visible()->not('about', 'changes') as $p): ?>
-		<li><a href="<?php echo $p->url() ?>"<?php echo ($p->isOpen()) ? ' class="active"' : '' ?>><?php echo html($p->title()) ?></a></li>
+		<li><a href="<?php echo $p->url() ?>"<?php echo ($p->isOpen()) ? ' class="active"' : '' ?>><?php if($p->menu() != '') { echo html($p->menu()); } else { echo html($p->title()); } ?></a></li>
 	<?php endforeach ?>
 		<li><a href="<?php echo $random->url() ?>">Random</a></li>
 		<li><a href="/search"<?php echo (page('search')->isOpen()) ? ' class="active"' : '' ?>>Search</a></li>
