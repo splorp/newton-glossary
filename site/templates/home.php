1e98b884e66a0043c21aa87167af89fc6eda2e38
Grant Hutchinson <grant@splorp.com>
2017-08-30 00:14:39 -0600

    Simplified home page, removed term and source counts

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,20 +1,10 @@
 <?php snippet('header') ?>
 <?php snippet('menu') ?>
 
-<?php
-	/* Nab the number of term and source pages */
-	$t = number_format(page('terms')->children()->count());
-	$s = number_format(page('sources')->children()->count());
-	/* Replace the term count placeholder in the page text */
-	$p = str_replace('_$t', $t, $page->text());
-	/* Replace the source count placeholder in the page text */
-	$pp = str_replace('_$s', $s, $p);
-?>
-
 <section>
 	<article>
 		<h1><?php echo html($page->title()) ?></h1>
-		<?php echo kirbytext($pp) ?>
+		<?php echo kirbytext($page->text()) ?>
 	</article>
 </section>
 
