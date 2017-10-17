4b11450a26e4e05e1c1b608baee66f407e3a44cc
Grant Hutchinson <grant@splorp.com>
2017-10-17 14:57:58 -0600

    Added link to sitemap, added alt text

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,8 +1,9 @@
 		<footer>
 			<nav>
 				<ul>
-					<li><a href="/about">About the Newton Glossary</a></li>
-					<li><a href="/changes">Change Log</a></li>
+					<li><a href="/about" alt="How this whole thing works, plus a little bit of history">About the Newton Glossary</a></li>
+					<li><a href="/changes" alt="A chronological list of major releases, updates, and improvements">Change Log</a></li>
+					<li><a href="/sitemap.xml" alt="An XML listing of all pages found on this site">Sitemap</a></li>
 				</ul>
 			</nav>
 			<?php echo kirbytext($site->copyright() . "<br />" . $site->colophon()) ?>
