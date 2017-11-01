002b8bf72838422c370cd7a74438cf94031255f3
Grant Hutchinson <grant@splorp.com>
2017-11-01 16:18:28 -0600

    Added site version

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,6 +1,7 @@
 		<footer>
 			<nav>
 				<ul>
+					<li>Version <?php echo ($site->version()) ?></li>
 					<li><a href="/about" title="<?php echo (page('about')->description()) ?>">About the Newton Glossary</a></li>
 					<li><a href="/changes" title="<?php echo (page('changes')->description()) ?>">Change Log</a></li>
 					<li><a href="/sitemap.xml" title="An XML listing of all pages found on this site.">Sitemap</a></li>
