b7b86d4340c7a7cf50e4f4d955b20918a9000a08
Grant Hutchinson <grant@splorp.com>
2017-11-15 16:36:02 -0700

    Entab’ing

--- site/config/config.php
+++ site/config/config.php
@@ -37,26 +37,26 @@ c::set('plugin.ga.id', 'UA-3915509-13');
 c::set('sitemap.include.invisible', true);
 
 c::set('meta-tags.default', function(Page $page, Site $site) {
-    return [
-        'title' => site()->title() . ' — ' . $page->title(),
-        'meta' => [
-            'description' => $page->isHomePage()
-                ? $site->description()
-                : ($page->description() != ''
+	return [
+		'title' => site()->title() . ' — ' . $page->title(),
+		'meta' => [
+			'description' => $page->isHomePage()
+				? $site->description()
+				: ($page->description() != ''
 					? $page->description()
 					: excerpt($page->text(), 175)
 				),
-            'keywords' => $site->keywords()
-        ],
-        'link' => [
-            'canonical' => $page->url()
-        ],
-        'og' => [
-            'title' => $page->title(),
-            'type' => 'website',
-            'site_name' => $site->title(),
-            'url' => $page->url()
-        ],
+			'keywords' => $site->keywords()
+		],
+		'link' => [
+			'canonical' => $page->url()
+		],
+		'og' => [
+			'title' => $page->title(),
+			'type' => 'website',
+			'site_name' => $site->title(),
+			'url' => $page->url()
+		],
 		'twitter' => [
 			'card' => 'summary',
 			'site' => '@newtonglossary',
@@ -70,8 +70,8 @@ c::set('meta-tags.default', function(Page $page, Site $site) {
 					: excerpt($page->text(), 175)
 				),
 			'image' => site()->url() . '/assets/images/twitter-image-800x800.png'
-        ]
-    ];
+		]
+	];
 });
 
 /*
