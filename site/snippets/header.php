5bf94ac11061d72d14130c6086b5d960faa8c639
Grant Hutchinson <grant@splorp.com>
2022-05-27 12:31:07 -0600

    Removed Google tag, changed to DNS verification

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -4,7 +4,6 @@
 		<meta charset="utf-8" />
 		<meta name="robots" content="index, follow" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<meta name="google-site-verification" content="3ebPkgj9vrzCZbR3OA-VrCvJmoUmn_SHAbpo3nHn_0U" />
 		<?php echo $page->metaTags() ?>
 		<link rel="shortcut icon" href="/favicon.ico" />
 		<link rel="apple-touch-icon-precomposed" sizes="57x57" href="/assets/devices/apple-touch-icon-57x57-precomposed.png" />
