aecc194f52564fbc4a9c5347b86316504e152d4f
Grant Hutchinson <grant@splorp.com>
2023-02-07 12:15:05 -0700

    Fixed list formatting

--- site/templates/latest.php
+++ site/templates/latest.php
@@ -7,9 +7,9 @@
 
 <section>
 	<article>
-		<h2><?php echo html($page->title()) ?></h2>
+		<h2 class="rslt"><?php echo html($page->title()) ?></h2>
 		<?php echo $page->text()->kirbytext() ?>
-		<ul class="rslt">
+		<ul>
 			<?php foreach($latest_pages as $latest): ?>
 			<li><a href="/<?php echo $latest ?>"><?php echo $latest->title()->html() ?></a><br />
 			<span class="mouseprint">New <?php echo rtrim($latest->parent()->slug(), "s") ?> added <?php echo $latest->date('d F Y') ?></span></li>
