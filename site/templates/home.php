ef96116f0cc9d9cd833602739f39baefd392ee7d
Grant Hutchinson <grant@splorp.com>
2025-05-13 14:53:03 -0600

    Added terms and source counts per issue #42

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,10 +1,18 @@
 <?php snippet('header') ?>
 <?php snippet('menu') ?>
 
+<?php
+	/* Nab the number of term and source pages */
+	$t = number_format(page('terms')->children()->count());
+	$s = number_format(page('sources')->children()->count());
+	/* Replace placeholders in the page text */
+	$p = str_replace(array('_$t','_$s'), array($t,$s), $page->text());
+?>
+
 <section>
 	<article>
 		<h2><?php echo html($page->title()) ?></h2>
-		<?php echo $page->text()->kirbytext() ?>
+		<?php echo kirbytext($p) ?>
 	</article>
 </section>
 
