42a0bc022e520aed93b10e3f20626c1ede1bd11d
Grant Hutchinson <grant@splorp.com>
2025-06-25 15:00:51 -0600

    Updated Kirby Paperback Export plugin to 2.0.7

--- site/plugins/paperback-export/snippets/export.php
+++ site/plugins/paperback-export/snippets/export.php
@@ -3,7 +3,7 @@
 	header('Content-Disposition: attachment; filename="' . $filename . '-paperback.txt"');
 	echo $title . PHP_EOL . PHP_EOL;
 	if ($description != '') { echo ($description . PHP_EOL . PHP_EOL); }
-	if ($version != '') { echo ('Version ' . $version . PHP_EOL . PHP_EOL); }
+	if ($release != '') { echo ('Release ' . $release . PHP_EOL . PHP_EOL); }
 	if ($datestamp != '') { echo ('Published ' . $datestamp . PHP_EOL . PHP_EOL); }
 	foreach ($pages as $page) :
 		snippet('paperback-export/content', compact('languages', 'page', 'prefix', 'fields'));
