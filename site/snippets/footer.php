d9ce266366ba27dd2f6bde618f0778cb703d6d30
Grant Hutchinson <grant@splorp.com>
2025-08-18 13:44:51 -0600

    Highlight footer links when pages are open

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -2,8 +2,8 @@
 			<nav>
 				<ul>
 					<li>Version <?php echo $site->release() ?></li>
-					<li><a href="<?php echo $site->url() ?>/about" title="<?php echo page('about')->description() ?>">About the Newton Glossary</a></li>
-					<li><a href="<?php echo $site->url() ?>/changes" title="<?php echo page('changes')->description() ?>">Changes</a></li>
+					<li><a href="<?php echo $site->url() ?>/about" title="<?php echo page('about')->description() ?>"<?php echo (page('about')->isOpen()) ? ' class="active"' : '' ?>>About the Newton Glossary</a></li>
+					<li><a href="<?php echo $site->url() ?>/changes" title="<?php echo page('changes')->description() ?>"<?php echo (page('changes')->isOpen()) ? ' class="active"' : '' ?>>Changes</a></li>
 					<li><a href="<?php echo $site->url() ?>/latest/feed" title="Subscribe to a feed of the latest terms and sources added to the glossary.">RSS</a></li>
 					<li><a href="<?php echo $site->url() ?>/sitemap" title="An XML listing of all pages found on this site.">Sitemap</a></li>
 				</ul>
